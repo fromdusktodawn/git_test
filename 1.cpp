550e0ffd8f1cdc84a943a80d1bfd17bfbd09ef05
fromdusktodawn <2563546603@qq.com>
2021-05-21 16:37:31 +0800

    push a new file

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: happyeveryday
+	> Mail: w2563546603@gmail.com
+	> Created Time: 2021年05月21日 星期五 16时36分52秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
